c868e7c99db1fa382d0ab8098f1f48f2d94188c5
Carles Company Soler <carlescs@gmail.com>
2020-12-24 16:59:56 +0100

    Remove blazor service.

--- frontend/Pages/AddUser.cshtml.cs
+++ frontend/Pages/AddUser.cshtml.cs
@@ -3,16 +3,19 @@ using CommonContracts.People;
 using frontend.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace frontend.Pages
 {
     public class AddUser : PageModel
     {
         private readonly PeopleService _peopleService;
+        private readonly ILogger<AddUser> _logger;
 
-        public AddUser(PeopleService peopleService)
+        public AddUser(PeopleService peopleService, ILogger<AddUser> logger)
         {
             _peopleService = peopleService;
+            _logger = logger;
         }
 
         [BindProperty]
@@ -21,6 +24,7 @@ namespace frontend.Pages
         public async Task<ActionResult> OnPost()
         {
             await _peopleService.SaveUser(Person.Name);
+            _logger.LogInformation("Saved User");
             return new RedirectToPageResult("Index");
         }
     }
--- frontend/Services/PeopleService.cs
+++ frontend/Services/PeopleService.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using CommonContracts.People;
+using Microsoft.Extensions.Logging;
 
 namespace frontend.Services
 {
     public class PeopleService
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<PeopleService> _logger;
 
-        public PeopleService(HttpClient httpClient)
+        public PeopleService(HttpClient httpClient, ILogger<PeopleService> logger)
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task<PersonContract[]> GetPeople()
